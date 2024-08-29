30a6824a6590313c0dd8b057fcd0bfaa84e281a7
RTiberiu <rociu.tibi@gmail.com>
2024-08-29 14:44:58 +0100

    Added valleys biome with domain warp

--- Source/UnrealTest/Private/PerlinNoiseSettings.cpp
+++ Source/UnrealTest/Private/PerlinNoiseSettings.cpp
@@ -7,19 +7,19 @@ APerlinNoiseSettings::APerlinNoiseSettings() {
 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = false;
 
-	// Flat-ish Valleys biome noise 
-	/*Octaves = { 2, 3, 4};
+	// Flat-ish Valleys biome noise - DONE
+	/*Octaves = { 2, 3, 4 };
 	Frequencies = { 0.007f, 0.01f, 0.003f };
 	Amplitudes = { 20, 15, 15 };
 	Lacunarity = { 2.0f, 2.0f, 2.0f };
 	Gain = { 0.5f, 0.5f, 0.5f };
-	WeightedStrength = { -1.7f, 0.0f, -0.7f };*/
+	WeightedStrength = { -1.3f, 0.0f, -0.7f };
 
 	DomainWarpAmp = { 80, 60, 100 };
 	DomainWarpFrequencies = { 0.03f, 0.06f, 0.06f };
 	DomainWarpOctaves = { 3, 3, 3 };
 	DomainWarpLacunarity = { 1.2f, 1.0f, 0.8f };
-	DomainWarpGain = { 1.0f, 1.0f, 1.0f };
+	DomainWarpGain = { 1.0f, 1.0f, 1.0f };*/
 
 	// Mountain biome noise - DONE
 	/*Octaves = { 3, 3, 5 };
